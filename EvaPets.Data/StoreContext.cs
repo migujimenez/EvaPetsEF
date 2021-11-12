9ac96d2c07dacd73e756f71288c25f6915dd80ba
migujimenez <migujimenez@uan.edu.co>
2021-11-12 16:14:10 -0500

    Add EF nugget and context class

---
+++ EvaPets.Data/StoreContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using EvaPets.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EvaPets.Data
+{
+    public class StoreContext :DbContext
+    {
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Employees> Employees { get; set; }
+    }
+}
