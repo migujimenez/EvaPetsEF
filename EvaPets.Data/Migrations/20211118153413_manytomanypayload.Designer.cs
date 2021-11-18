d66e7e94fca1c59d9665189490a8560f3fa12aaf
migujimenez <migujimenez@uan.edu.co>
2021-11-18 10:54:13 -0500

    Add migrations with payload

--- EvaPets.Data/Migrations/20211118153413_manytomanypayload.Designer.cs
+++ EvaPets.Data/Migrations/20211118153413_manytomanypayload.Designer.cs
@@ -4,14 +4,16 @@ using EvaPets.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EvaPets.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20211118153413_manytomanypayload")]
+    partial class manytomanypayload
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EvaPets.Data/Migrations/20211118153413_manytomanypayload.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EvaPets.Data.Migrations
+{
+    public partial class manytomanypayload : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CityId",
+                table: "CitiesStore",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOpened",
+                table: "CitiesStore",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "getdate()");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "CitiesStore");
+
+            migrationBuilder.DropColumn(
+                name: "DateOpened",
+                table: "CitiesStore");
+        }
+    }
+}
