7e791349abf7e34e4f5672d9abb23b850d016e36
migujimenez <migujimenez@uan.edu.co>
2021-11-19 10:05:31 -0500

    Add diferent models

--- EvaPets.UI/Program.cs
+++ EvaPets.UI/Program.cs
@@ -15,14 +15,15 @@ namespace EvaPets.UI
             WriteLine("Hello EvaPets Store!");
             _storeContext.Database.EnsureCreated();
             GetStore("Before Add");
-            AddStore("YourPlanetPet", "The Duck");
+            //AddStoresByName("Store1", "Store2", "Store3", "Store4");
+            AddStoresAndCities();
             GetStore("After Add:");
             Write("Press any key...");
             ReadKey();
 
         }
 
-        private static void AddStore(params string[] names)
+        private static void AddStoresByName(params string[] names)
         {
             /*var store = new Store { Name = "AllFarm" };
             _storeContext.Stores.Add(store);
@@ -32,6 +33,18 @@ namespace EvaPets.UI
             {
                 _storeContext.Stores.Add(new Store { Name = store });
             }
+            _storeContext.SaveChanges();
+        }
+
+
+        private static void AddStoresAndCities()
+        {
+            _storeContext.Stores.AddRange(
+                new Store { Name = "Tower Records" },
+                new Store { Name = "MyMusic" });
+            _storeContext.Cities.AddRange(
+                new Cities { Name = "Bogota"},
+                new Cities { Name = "London" });
             _storeContext.SaveChanges();
         }
 
